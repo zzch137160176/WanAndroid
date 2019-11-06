a8e6fbb0279932d816c0eeef4b3385fea7f99489
zzch137160176 <zzch9qnby@163.com>
2019-11-06 18:15:30 +0800

    首页底部按键功能实现

--- app/src/main/java/per/zzch/wanandroid/view/MainActivity.kt
+++ app/src/main/java/per/zzch/wanandroid/view/MainActivity.kt
@@ -15,19 +15,44 @@ class MainActivity : BaseBindingActivity<MainVM, MainB>(R.layout.activity_main) 
     private val mResourceFragment = ResourceFragment.newInstance()
 
     override fun initEventAndData() {
-        mBinding.mViewPager.adapter = object : FragmentPagerAdapter(
-            supportFragmentManager,
-            BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
-        ) {
-            override fun getItem(position: Int): Fragment {
-                return getFragment(position)
-            }
+        with(mBinding) {
+            mViewPager.adapter = object : FragmentPagerAdapter(
+                supportFragmentManager,
+                BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+            ) {
+                override fun getItem(position: Int): Fragment {
+                    return getFragment(position)
+                }
 
-            override fun getCount(): Int {
-                return 4
+                override fun getCount(): Int {
+                    return 4
+                }
+            }
+            mViewPager.offscreenPageLimit = 3
+
+
+
+            mPage1.setOnClickListener {
+                clearTab()
+                mPage1.setBackgroundColor(resources.getColor(R.color.check))
+                mViewPager.currentItem = 0
+            }
+            mPage2.setOnClickListener {
+                clearTab()
+                mPage2.setBackgroundColor(resources.getColor(R.color.check))
+                mViewPager.currentItem = 1
+            }
+            mPage3.setOnClickListener {
+                clearTab()
+                mPage3.setBackgroundColor(resources.getColor(R.color.check))
+                mViewPager.currentItem = 2
+            }
+            mPage4.setOnClickListener {
+                clearTab()
+                mPage4.setBackgroundColor(resources.getColor(R.color.check))
+                mViewPager.currentItem = 3
             }
         }
-        mBinding.mViewPager.offscreenPageLimit = 3
     }
 
     override fun onCreate() {
@@ -35,12 +60,25 @@ class MainActivity : BaseBindingActivity<MainVM, MainB>(R.layout.activity_main) 
     }
 
     private fun getFragment(position: Int): Fragment {
-        return when(position) {
+        return when (position) {
             0 -> mBannerFragment
             1 -> mArticleFragment
             2 -> mProjectFragment
             3 -> mResourceFragment
             else -> mBannerFragment
+        }
+    }
+
+    private fun clickTab() {
+
+    }
+
+    private fun clearTab() {
+        with(mBinding) {
+            mPage1.setBackgroundColor(resources.getColor(R.color.white))
+            mPage2.setBackgroundColor(resources.getColor(R.color.white))
+            mPage3.setBackgroundColor(resources.getColor(R.color.white))
+            mPage4.setBackgroundColor(resources.getColor(R.color.white))
         }
     }
 }
